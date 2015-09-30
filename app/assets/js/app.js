8157702e4b011cc9faf4fdb23cee91994e4e1e77
Lucas Fayolle <lfayolle1@gmail.com>
2015-09-30 12:35:22 -0700

    responsive css

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
  	initPage();
+
+ 	$('.square').hover(hoverIn, hoverOut);
  });
 
 
@@ -8,4 +10,20 @@ function initPage(){
 
 	var slider = $.simpleSlider();
 
+	TweenMax.set('.square .overlay', {opacity:0,display:'none'});
+	TweenMax.set('.square .overlay h4, .square .overlay p', {opacity:0,y:20});
+
+}
+
+function hoverIn() {
+	var overlay = $(this).find('.overlay');
+	var overlayContent = overlay.find('h4, p');
+	TweenMax.to(overlay, 0.25, {opacity:1,display:'block',ease:Power2.easeOut});
+	TweenMax.staggerTo(overlayContent, 0.25, {opacity:1,y:0,ease:Power2.easeOut,delay:0.25}, 0.1);
+}
+function hoverOut() {
+	var overlay = $(this).find('.overlay');
+	var overlayContent = overlay.find('h4, p');
+	TweenMax.staggerTo(overlayContent, 0.25, {opacity:0,y:20,ease:Power2.easeOut}, 0.1);
+	TweenMax.to(overlay, 0.25, {opacity:0,display:'none',ease:Power2.easeOut,delay:0.25});
 }
